3988609c871e97bb8de8938791d724abfcca34db
Francesco <inerba@gmail.com>
2023-01-30 10:18:47 +0100

    fix icon fill

--- download.sh
+++ download.sh
@@ -7,26 +7,10 @@ echo "Copying the SVGs..."
 cp -r MaterialDesign/svg/* resources/svg/
 
 echo "Modifying the SVGs..."
-# find resources/svg -name "*.svg" -exec sed -i '/<svg/{/fill/!s/<svg/<svg fill="currentColor"/;}' {} +
+find resources/svg -name "*.svg" -exec sed -i '/<svg/{/fill/!s/<svg/<svg fill="currentColor"/;}' {} +
 
-find . -name "*-outline.svg" -exec sed -i 's/<svg/<svg fill="none" stroke="currentColor"/' {} +
-find . -name "*.svg" ! -name "*-outline.svg" -exec sed -i 's/<svg/<svg fill="currentColor"/' {} +
-
-
-# # loop through each file in the directory
-# for file in "resources/svg"/*.svg; do
-#   # get the file name without the path and extension
-#   filename=$(basename "$file" .svg)
-
-#   # check if the file name ends with "-outline"
-#   if [[ "$filename" == *"-outline" ]]; then
-#     # add fill="none" and stroke="currentColor" properties to the file
-#     sed -i 's/<svg/<svg fill="none" stroke="currentColor"/' "$file"
-#   else
-#     # add fill="currentColor" property to the file
-#     sed -i 's/<svg/<svg fill="currentColor"/' "$file"
-#   fi
-# done
+# find . -name "*-outline.svg" -exec sed -i 's/<svg/<svg fill="none" stroke="currentColor"/' {} +
+# find . -name "*.svg" ! -name "*-outline.svg" -exec sed -i 's/<svg/<svg fill="currentColor"/' {} +
 
 echo "Removing the cloned repo..."
 rm -rf MaterialDesign/
